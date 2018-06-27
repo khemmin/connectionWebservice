e9e819b9e2f51fb0d6b3b5e1d631da5d3dfc59e0
Kemin Suenggittawisuthi <khemmin@macbook-pro-kemin.local>
2018-06-27 13:19:15 +0700

    Initial Commit

---
+++ connectionWebservice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  connectionWebservice
+//
+//  Created by Kemin Suenggittawisuthi on 27/6/61.
+//  Copyright © พ.ศ. 2561 Kemin Suenggittawisuthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ connectionWebserviceTests/connectionWebserviceTests.swift
@@ -0,0 +1,36 @@
+//
+//  connectionWebserviceTests.swift
+//  connectionWebserviceTests
+//
+//  Created by Kemin Suenggittawisuthi on 27/6/61.
+//  Copyright © พ.ศ. 2561 Kemin Suenggittawisuthi. All rights reserved.
+//
+
+import XCTest
+@testable import connectionWebservice
+
+class connectionWebserviceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
